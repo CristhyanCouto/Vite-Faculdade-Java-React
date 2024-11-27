38927662687e5f7d9f39917a100b0cef1d62b743
Cristhyan Couto <cristhyancouto@gmail.com>
2024-11-27 15:22:55 -0300

    update-client: header removed from login page

--- client/src/components/common/login.tsx
+++ client/src/components/common/login.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Page from "./page";
 import {
   FormControl,
   InputLabel,
@@ -14,8 +13,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <Page>
-      <div className="container mx-auto mt-8 justify-center flex items-center flex-col h-[500px]">
+      <div className="container mx-auto mt-8 justify-center flex items-center flex-col h-screen">
         <h1 className="text-3xl font-bold text-center mb-4">Login</h1>
         <div className="flex flex-col gap-2 w-full items-center justify-center">
           <Grid2>
@@ -52,6 +50,5 @@ export default function Login() {
         <Button onClick={() => console.log(email, password)}>Fake Login</Button>
         <Button component={Link} to="/home">Página de Tarefas</Button>
       </div>
-    </Page>
   );
 }
